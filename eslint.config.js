f9cc08e6e5fa412c6b21c2467dd60a289d69bb7b
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2024-12-01 00:12:14 +0100

    change ecmaversion to 2024

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,8 @@ import prettier from "eslint-config-prettier";
 
 const __dirname = new URL(".", import.meta.url).pathname.slice(0, -1);
 
+foxkit.base.languageOptions.ecmaVersion = 2024;
+
 /**
  * @see https://github.com/foxkit-js/eslint-config-foxkit/ for more information
  */
