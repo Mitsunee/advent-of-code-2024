3c73bc5439245af8bf7d6bc57e9857d4b42cdac7
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2024-12-16 19:53:50 +0100

    add: Direction to coordinate shared utils

--- src/shared/coordinates.ts
+++ src/shared/coordinates.ts
@@ -35,3 +35,57 @@ export function stringToPosition(str: PositionStr) {
   }
   return pos;
 }
+
+export interface Direction extends Position {
+  direction: "up" | "right" | "down" | "left";
+}
+
+export type DirectionStr = ReturnType<typeof directionToString>;
+
+export const directionMovement: Record<Direction["direction"], Position> = {
+  up: { x: 0, y: -1 },
+  right: { x: 1, y: 0 },
+  down: { x: 0, y: 1 },
+  left: { x: -1, y: 0 }
+};
+
+export const directionArrow: Record<Direction["direction"], string> = {
+  up: "↑",
+  right: "→",
+  down: "↓",
+  left: "←"
+};
+
+/**
+ * Converts Direction object to standardized string representation
+ * @param pos Direction
+ * @returns Direction as string
+ */
+export function directionToString(pos: Direction) {
+  return `[${pos.x},${pos.y},${pos.direction}]` as const;
+}
+
+function dirStrIsValid(dirStr: string): dirStr is Direction["direction"] {
+  return (
+    dirStr == "up" || dirStr == "right" || dirStr == "down" || dirStr == "left"
+  );
+}
+
+/**
+ * Recreates Direction object from string representation
+ * @param str String
+ * @returns Direction Object
+ */
+export function stringToDirection(str: DirectionStr) {
+  const [xStr, yStr, dir] = str.slice(1, -1).split(",");
+  if (!dirStrIsValid(dir)) {
+    throw new Error(`Could not create Direction from string: '${str}'`);
+  }
+
+  const pos: Direction = { x: Number(xStr), y: Number(yStr), direction: dir };
+  if (isNaN(pos.x) || isNaN(pos.y)) {
+    throw new Error(`Could not create Direction from string: '${str}'`);
+  }
+
+  return pos;
+}
